844d8f59062b74d4ad662d5efc06c65bd057fcfa
nullzeiger <ivan.guerreschi.dev@gmail.com>
2024-07-15 17:55:00 +0200

    Add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 Ivan Guerreschi. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 use anyhow::Result;
 use rss::Channel;
 use std::io;
